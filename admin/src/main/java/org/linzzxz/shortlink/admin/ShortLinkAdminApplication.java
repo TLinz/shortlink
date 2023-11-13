585a0910b25a7564f8253b88d6456f41176024fd
TLinz <linz1114@163.com>
2023-11-13 17:33:47 +0100

    创建用户表，完善用户信息查询功能

--- admin/src/main/java/org/linzzxz/shortlink/admin/ShortLinkAdminApplication.java
+++ admin/src/main/java/org/linzzxz/shortlink/admin/ShortLinkAdminApplication.java
@@ -1,9 +1,12 @@
 package org.linzzxz.shortlink.admin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// 具体到包路径，减少扫描时间提升性能
+@MapperScan("org.linzzxz.shortlink.admin.dao.mapper")
 public class ShortLinkAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortLinkAdminApplication.class, args);
