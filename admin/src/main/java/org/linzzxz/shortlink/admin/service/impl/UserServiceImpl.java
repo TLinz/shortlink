482962e9d16c9a54d0a2d46b12d8da47e946971d
TLinz <linz1114@163.com>
2023-11-14 16:36:14 +0100

    集成布隆过滤器

--- admin/src/main/java/org/linzzxz/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/org/linzzxz/shortlink/admin/service/impl/UserServiceImpl.java
@@ -3,12 +3,14 @@ package org.linzzxz.shortlink.admin.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.linzzxz.shortlink.admin.common.convention.exception.ClientException;
 import org.linzzxz.shortlink.admin.common.enums.UserErrorCodeEnum;
 import org.linzzxz.shortlink.admin.dao.entity.UserDO;
 import org.linzzxz.shortlink.admin.dao.mapper.UserMapper;
 import org.linzzxz.shortlink.admin.dto.resp.UserRespDTO;
 import org.linzzxz.shortlink.admin.service.UserService;
+import org.redisson.api.RBloomFilter;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +18,10 @@ import org.springframework.stereotype.Service;
  * 用户接口实现层
  */
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
+
+    private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
 
     @Override
     public UserRespDTO getUserByUsername(String username) {
@@ -33,9 +38,6 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
 
     @Override
     public boolean hasUserName(String username) {
-        LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
-                .eq(UserDO::getUsername, username);
-        UserDO userDO = baseMapper.selectOne(queryWrapper);
-        return userDO != null;
+        return userRegisterCachePenetrationBloomFilter.contains(username);
     }
 }
