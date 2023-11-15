f7026989d5ab4f0e0d7f196bfa8ba3099e63d0cf
TLinz <linz1114@163.com>
2023-11-15 21:21:46 +0100

    初始化短链接分组功能

---
+++ admin/src/main/java/org/linzzxz/shortlink/admin/controller/GroupController.java
@@ -0,0 +1,15 @@
+package org.linzzxz.shortlink.admin.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.linzzxz.shortlink.admin.service.GroupService;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 短链接分组控制层
+ */
+@RestController
+@RequiredArgsConstructor
+public class GroupController {
+
+    private final GroupService groupService;
+}
---
+++ admin/src/main/java/org/linzzxz/shortlink/admin/service/GroupService.java
@@ -0,0 +1,10 @@
+package org.linzzxz.shortlink.admin.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.linzzxz.shortlink.admin.dao.entity.GroupDO;
+
+/**
+ * 短链接分组接口层
+ */
+public interface GroupService extends IService<GroupDO> {
+}
