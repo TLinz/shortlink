0e46659288764cbf6e2ceb9d919b6cd8e1003f15
TLinz <linz1114@163.com>
2023-11-16 23:47:35 +0100

    更改短链接表short_uri字段编码，区分大小写

--- sql/link_tables.sql
+++ sql/link_tables.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `t_link` (
-                          `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+                          `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',
                           `domain` varchar(128) DEFAULT NULL COMMENT '域名',
-                          `short_uri` varchar(8) DEFAULT NULL COMMENT '短链接',
+                          `short_uri` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '短链接',
                           `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
                           `origin_url` varchar(1024) DEFAULT NULL COMMENT '原始链接',
-                          `click_num` int(11) DEFAULT 0 COMMENT '点击量',
+                          `click_num` int DEFAULT '0' COMMENT '点击量',
                           `gid` varchar(32) DEFAULT NULL COMMENT '分组标识',
                           `enable_status` tinyint(1) DEFAULT NULL COMMENT '启用标识 0：未启用 1：已启用',
                           `created_type` tinyint(1) DEFAULT NULL COMMENT '创建类型 0：控制台 1：接口',
@@ -16,4 +16,4 @@ CREATE TABLE `t_link` (
                           `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',
                           PRIMARY KEY (`id`),
                           UNIQUE KEY `idx_unique_full_short_uri` (`full_short_url`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=1725280835751387139 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
