73af481ca938341147257cd7f695f13a83cf0cf5
TLinz <linz1114@163.com>
2023-11-20 14:35:23 +0100

    feature: 修正跳转表结构

--- project/src/test/java/org/linzzxz/shortlink/project/test/LinkGotoTableShardingTest.java
+++ project/src/test/java/org/linzzxz/shortlink/project/test/LinkGotoTableShardingTest.java
@@ -3,10 +3,11 @@ package org.linzzxz.shortlink.project.test;
 public class LinkGotoTableShardingTest {
 
     public static final String SQL = "CREATE TABLE `t_link_goto_%d` (\n" +
-            "                               `id` bigint unsigned NOT NULL AUTO_INCREMENT,\n" +
-            "                               `short_uri` varchar(8) DEFAULT NULL,\n" +
-            "                               `gid` varchar(32) DEFAULT NULL,\n" +
-            "                               PRIMARY KEY (`id`)\n" +
+            "                                 `id` bigint unsigned NOT NULL AUTO_INCREMENT,\n" +
+            "                                 `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',\n" +
+            "                                 `gid` varchar(32) DEFAULT NULL COMMENT '分组标识',\n" +
+            "                                 PRIMARY KEY (`id`),\n" +
+            "                                 UNIQUE KEY `full_short_url` (`full_short_url`)\n" +
             ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;";
 
     public static void main(String[] args) {
--- sql/singleTbl/link_goto.sql
+++ sql/singleTbl/link_goto.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `t_link_goto` (
-                               `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-                               `short_uri` varchar(8) DEFAULT NULL,
-                               `gid` varchar(32) DEFAULT NULL,
-                               PRIMARY KEY (`id`)
+                                 `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+                                 `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
+                                 `gid` varchar(32) DEFAULT NULL COMMENT '分组标识',
+                                 PRIMARY KEY (`id`),
+                                 UNIQUE KEY `full_short_url` (`full_short_url`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
