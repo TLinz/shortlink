34b60397edab600a061e0f8f96c40d9654a02ccd
TLinz <linz1114@163.com>
2023-11-22 14:07:03 +0100

    实现短链接基础监控功能：地区访问数据

--- project/src/main/java/org/linzzxz/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/org/linzzxz/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -7,6 +7,9 @@ import cn.hutool.core.lang.UUID;
 import cn.hutool.core.text.StrBuilder;
 import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
@@ -23,9 +26,11 @@ import org.linzzxz.shortlink.project.common.convention.exception.ClientException
 import org.linzzxz.shortlink.project.common.convention.exception.ServiceException;
 import org.linzzxz.shortlink.project.common.enums.ValidDateTypeEnum;
 import org.linzzxz.shortlink.project.dao.entity.LinkAccessStatsDO;
+import org.linzzxz.shortlink.project.dao.entity.LinkLocaleStatsDO;
 import org.linzzxz.shortlink.project.dao.entity.ShortLinkDO;
 import org.linzzxz.shortlink.project.dao.entity.ShortLinkGotoDO;
 import org.linzzxz.shortlink.project.dao.mapper.LinkAccessStatsMapper;
+import org.linzzxz.shortlink.project.dao.mapper.LinkLocaleStatsMapper;
 import org.linzzxz.shortlink.project.dao.mapper.ShortLinkGotoMapper;
 import org.linzzxz.shortlink.project.dao.mapper.ShortLinkMapper;
 import org.linzzxz.shortlink.project.dto.req.ShortLinkCreateReqDTO;
@@ -40,6 +45,7 @@ import org.linzzxz.shortlink.project.toolkit.LinkUtil;
 import org.redisson.api.RBloomFilter;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -50,6 +56,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.linzzxz.shortlink.project.common.constant.RedisKeyConstant.*;
+import static org.linzzxz.shortlink.project.common.constant.ShortLinkConstant.AMAP_REMOTE_URL;
 
 /**
  * 短链接接口实现层
@@ -64,6 +71,10 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
     private final StringRedisTemplate stringRedisTemplate;
     private final RedissonClient redissonClient;
     private final LinkAccessStatsMapper linkAccessStatsMapper;
+    private final LinkLocaleStatsMapper linkLocaleStatsMapper;
+
+    @Value("${short-link.stats.locale.amap-key}")
+    private String statsLocaleAmapKey;
 
     @SneakyThrows
     @Override
@@ -173,6 +184,28 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                     .fullShortUrl(fullShortUrl)
                     .build();
             linkAccessStatsMapper.shortLinkStats(linkAccessStatsDO);
+            Map<String, Object> localeParamMap = new HashMap<>();
+            localeParamMap.put("key", statsLocaleAmapKey);
+            localeParamMap.put("ip", remoteAddr);
+            String localeResultStr = HttpUtil.get(AMAP_REMOTE_URL, localeParamMap);
+            JSONObject localeResultObj = JSON.parseObject(localeResultStr);
+            String infoCode = localeResultObj.getString("infocode");
+            LinkLocaleStatsDO linkLocaleStatsDO;
+            if (StrUtil.isNotBlank(infoCode) && StrUtil.equals(infoCode, "10000")) {
+                String province = localeResultObj.getString("province");
+                boolean unknownFlag = StrUtil.equals(province, "[]");
+                linkLocaleStatsDO = LinkLocaleStatsDO.builder()
+                        .province(unknownFlag ? "未知" : province)
+                        .city(unknownFlag ? "未知" : localeResultObj.getString("city"))
+                        .adcode(unknownFlag ? "未知" : localeResultObj.getString("adcode"))
+                        .country(unknownFlag ? "未知" : "中国") // 默认国内访问
+                        .cnt(1)
+                        .fullShortUrl(fullShortUrl)
+                        .gid(gid)
+                        .date(new Date())
+                        .build();
+                linkLocaleStatsMapper.shortLinkLocaleStats(linkLocaleStatsDO);
+            }
         } catch (Throwable ex) {
             log.error("短链接访问量统计异常");
         }
